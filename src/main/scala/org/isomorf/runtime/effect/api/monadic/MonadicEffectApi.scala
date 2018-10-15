770d10208fb8c6a30ada1fac2eff0f47dc8db4d7
Brian Kent <brian.kent@isomorf.io>
2018-10-14 22:44:58 -0400

    finally fixing signatures

--- src/main/scala/org/isomorf/runtime/effect/api/monadic/MonadicEffectApi.scala
+++ src/main/scala/org/isomorf/runtime/effect/api/monadic/MonadicEffectApi.scala
@@ -7,12 +7,12 @@ import org.isomorf.foundation.runtime.effects.plugins.EffectProvider
 trait MonadicEffectApi extends EffectProvider {
 
   // START MONADIC
-  def monadicInject[A](a: A): A
+  def monadicInject[A](a: A): RTEffect[A]
 
-  def monadicBind[A, B](m: RTEffect[A], f: A => RTEffect[B]): B
+  def monadicBind[A, B](m: RTEffect[A], f: A => RTEffect[B]): RTEffect[B]
 
-  def monadicAndThen[A, B](ma: RTEffect[A], mb: RTEffect[B]): B
+  def monadicAndThen[A, B](ma: RTEffect[A], mb: RTEffect[B]): RTEffect[B]
   // END MONADIC
 
-  def monadicZip[A, B](ma: RTEffect[A], mb: RTEffect[B]): (A, B)
+  def monadicZip[A, B](ma: RTEffect[A], mb: RTEffect[B]): RTEffect[(A, B)]
 }
